fa714024d1a821d3e1d0dea679555e4e0886cc63
Alexander Taborda Acosta <alextab93@gmail.com>
2017-01-30 17:20:55 -0500

    change display method

--- app/src/main/java/com/example/laboratorio/actividadclase1/MainActivity.java
+++ app/src/main/java/com/example/laboratorio/actividadclase1/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,69 +28,11 @@ public class MainActivity extends AppCompatActivity {
         second_grade = (EditText) findViewById(R.id.second_grade);
         third_grade = (EditText) findViewById(R.id.third_grade);
         fourth_grade = (TextView) findViewById(R.id.fourth_grade);
-
-        first_grade.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                if(!(first_grade.getText().toString().equals("")) && !(second_grade.getText().toString().equals("")) && !(third_grade.getText().toString().equals(""))){
-                    fourth_grade.setText(String.valueOf(calculateNote(Double.parseDouble(first_grade.getText().toString()),Double.parseDouble(second_grade.getText().toString()),Double.parseDouble(third_grade.getText().toString()))));
-                }
-            }
-        });
-
-        second_grade.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                if(!(first_grade.getText().toString().equals("")) && !(second_grade.getText().toString().equals("")) && !(third_grade.getText().toString().equals(""))){
-                    fourth_grade.setText(String.valueOf(calculateNote(Double.parseDouble(first_grade.getText().toString()),Double.parseDouble(second_grade.getText().toString()),Double.parseDouble(third_grade.getText().toString()))));
-                }
-
-            }
-        });
-
-        third_grade.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                if(!(first_grade.getText().toString().equals("")) && !(second_grade.getText().toString().equals("")) && !(third_grade.getText().toString().equals(""))){
-                    fourth_grade.setText(String.valueOf(calculateNote(Double.parseDouble(first_grade.getText().toString()),Double.parseDouble(second_grade.getText().toString()),Double.parseDouble(third_grade.getText().toString()))));
-                }
-
-            }
-        });
     }
 
-
+    public void sendMessage(View view){
+        if(!(first_grade.getText().toString().equals("")) && !(second_grade.getText().toString().equals("")) && !(third_grade.getText().toString().equals(""))){
+            fourth_grade.setText(String.valueOf(calculateNote(Double.parseDouble(first_grade.getText().toString()),Double.parseDouble(second_grade.getText().toString()),Double.parseDouble(third_grade.getText().toString()))));
+        }
+    }
 }
